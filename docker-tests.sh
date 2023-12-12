c8b9194c6a0d69e7188049ba5725eaa889b02d5c
euiyounghwang <marieuig@gmail.com>
2023-12-12 15:50:23 -0600

    Update docker-compose.yml

--- docker-tests.sh
+++ docker-tests.sh
@@ -4,7 +4,7 @@ set -eu
 
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-docker run --rm --platform linux/amd64 -it -d \
+docker run --rm -it --platform linux/amd64 -it -d \
   --name fn-elasticsearch-api-test --publish 8889:8889 --expose 8889 \
   --network bridge \
   -e ES_HOST=http://host.docker.internal:9209 \
