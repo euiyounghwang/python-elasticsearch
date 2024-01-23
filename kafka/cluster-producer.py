d44fa330f14cae9fe053cf82bad677488b39d5bb
euiyounghwang <marieuig@gmail.com>
2024-01-22 23:59:46 -0600

    Update Kafak argument with Docker

--- kafka/cluster-producer.py
+++ kafka/cluster-producer.py
@@ -1,9 +1,17 @@
 from kafka import KafkaProducer
 
-brokers = ['localhost:29092', 'localhost:39092']
-topic = 'test-topic'
 
-producer = KafkaProducer(bootstrap_servers=brokers)
 
-producer.send(topic, b'Hello, World!')
-producer.flush()
+def produce_kafka():
+    brokers = ['localhost:29092', 'localhost:39092']
+    topic = 'test-topic'
+
+    producer = KafkaProducer(bootstrap_servers=brokers)
+
+    for _ in range(3):
+        producer.send(topic, b'Hello, World!')
+        producer.flush()
+    
+    
+if __name__ == "__main__":
+    produce_kafka()
