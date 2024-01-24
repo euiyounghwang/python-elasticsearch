e60b011c76d292b6607154dd069121afb21db103
euiyounghwang <marieuig@gmail.com>
2024-01-23 23:41:59 -0600

    Add Kafka-logstash

--- kafka/cluster-consumer.py
+++ kafka/cluster-consumer.py
@@ -11,6 +11,7 @@ def consumer_kafka():
     for message in consumer:
         print(message, message.value, message.value.decode("utf-8"))
     
+    
         
 if __name__ == "__main__":
     consumer_kafka()
