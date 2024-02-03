4eab655f835cb8e9943388c6b291f820ee23cdc8
euiyounghwang <marieuig@gmail.com>
2024-02-03 17:06:50 -0600

    Update

--- Curator/curator-run-job.sh
+++ Curator/curator-run-job.sh
@@ -13,6 +13,7 @@ source $filepath/.venv/bin/activate
 
 # $filepath/.venv/bin/curator --config ./Curator/curator-config.yml --dry-run ./Curator/delete-indices.yml
 # Test
-curator --config ./Curator/curator-config.yml --dry-run ./Curator/delete-indices.yml
+# curator --config ./Curator/curator-config.yml --dry-run ./Curator/delete-indices.yml
+curator --config $SCRIPTDIR/curator-config.yml --dry-run $SCRIPTDIR/delete-indices.yml
 # Run
 # curator --config ./Curator/curator-config.yml ./Curator/delete-indices.yml
