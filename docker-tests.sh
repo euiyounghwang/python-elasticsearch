f9b59c441602194a4b24b6f9075c7f1ba1cd5e0c
euiyounghwang <marieuig@gmail.com>
2024-02-26 21:01:26 -0600

    Update elasticsearch url

--- docker-tests.sh
+++ docker-tests.sh
@@ -7,6 +7,6 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 docker run --rm -it --platform linux/amd64 -it -d \
   --name fn-elasticsearch-api-test --publish 8889:8889 --expose 8889 \
   --network bridge \
-  -e ES_HOST=http://host.docker.internal:9209 \
+  -e ES_HOST=http://host.docker.internal:9203 \
   -v "$SCRIPTDIR:/app/FN-Basic-Services/" \
   fn-elasticsearch-api:test
